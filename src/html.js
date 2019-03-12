a5d395dbc2a20b7018b4dde317c494c4defd0a83
Justin Harr <justin@Justins-MBP.lan>
2019-03-12 06:30:55 -0400

    added google verify

--- src/html.js
+++ src/html.js
@@ -5,7 +5,8 @@ export default class HTML extends React.Component {
   render() {
     return (
       <html {...this.props.htmlAttributes}>
-        <head>
+        <
+        <meta name="google-site-verification" content="dlxJAgxryHAE28GnSRGmXKflRVAilzhGYa" />
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
